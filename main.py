f079a4d39b8f01d6ebc7adb9f08ea14f0cf4a166
shayanlhm <95292110+shayanlhm@users.noreply.github.com>
2025-07-28 13:34:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def proxy(path):
     return response
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
